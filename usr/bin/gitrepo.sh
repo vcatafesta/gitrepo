0372f90bf37a1c30f638a4c4844f90131f86044b
vcatafesta <vcatafesta@gmail.com>
2024-09-10 01:26:02 -0400

    implementação do parametro color

--- usr/bin/gitrepo.sh
+++ usr/bin/gitrepo.sh
@@ -989,7 +989,7 @@ for arg in "$@"; do
 done
 
 # Cores e estilos
-[[ $nocolor == false ]] && unset_varcolors || set_varcolors
+[[ $nocolor == "true" ]] && unset_varcolors || set_varcolors
 
 [[ "$1" = @(-V|--version) ]] && {
 	sh_version
